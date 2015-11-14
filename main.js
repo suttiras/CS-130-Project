b696fcc3ffecb24b189b20874181bf20217fe0a3
Sandra Suttiratana <suttiras@ucla.edu>
2015-11-14 01:46:37 -0800

    Added a sample function

--- main.js
+++ main.js
@@ -5,3 +5,20 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("search_groups", function(request, response) {
+  var query = new Parse.Query("Review");
+  query.equalTo("movie", request.params.movie);
+  query.find({
+    success: function(results) {
+      var sum = 0;
+      for (var i = 0; i < results.length; ++i) {
+        sum += results[i].get("stars");
+      }
+      response.success(sum / results.length);
+    },
+    error: function() {
+      response.error("movie lookup failed");
+    }
+  });
+});
